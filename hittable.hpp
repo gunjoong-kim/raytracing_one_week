dab143645fb5e67ac18888bf08afeeb69730d2da
Gunjoong Kim <rjsoownd0419@gmail.com>
2023-07-21 18:57:55 +0900

    add diffuse model

--- hittable.hpp
+++ hittable.hpp
@@ -9,9 +9,9 @@ struct hit_record {
     double t;
     bool front_face;
 
-    inline void set_face_normal(const ray &r, const vec3 &ourward_normal) {
-        front_face = dot(r.direction(), ourward_normal) < 0;
-        normal = front_face ? ourward_normal : -ourward_normal;
+    inline void set_face_normal(const ray &r, const vec3 &outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
     }
 };
 
