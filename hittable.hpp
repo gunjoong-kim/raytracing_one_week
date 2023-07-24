2f9d103879dc4f866229d49205db6f81a14e4170
Gunjoong Kim <rjsoownd0419@gmail.com>
2023-07-24 18:16:21 +0900

    final model

--- hittable.hpp
+++ hittable.hpp
@@ -1,11 +1,14 @@
 #ifndef HITTABLE_HPP
 # define HITTABLE_HPP
 
-# include "ray.hpp"
+# include "rtweekend.hpp"
+
+class material;
 
 struct hit_record {
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat_ptr;
     double t;
     bool front_face;
 
